9fb81ce010f9bcb2f6fb76bff74394114a8b6c63
Jakub Zelenka <bukka@php.net>
2024-02-20 20:24:56 +0000

    Add initial response expectation action execution

--- run/actions/expect/response.go
+++ run/actions/expect/response.go
@@ -1,11 +1,14 @@
 package expect
 
 import (
+	"errors"
 	"fmt"
 	"github.com/bukka/wst/app"
 	"github.com/bukka/wst/conf/types"
+	"github.com/bukka/wst/run/actions/request"
 	"github.com/bukka/wst/run/instances/runtime"
 	"github.com/bukka/wst/run/services"
+	"regexp"
 )
 
 type ResponseAction struct {
@@ -53,7 +56,57 @@ func (m *ResponseExpectationActionMaker) MakeAction(
 	}, nil
 }
 
-func (a ResponseAction) Execute(runData runtime.Data) (bool, error) {
+func (a *ResponseAction) Execute(runData runtime.Data) (bool, error) { // Retrieve ResponseData from runData using the Request string as the key.
+	data, ok := runData.Load(a.Request)
+	if !ok {
+		return false, errors.New("response data not found")
+	}
+
+	responseData, ok := data.(request.ResponseData)
+	if !ok {
+		return false, errors.New("invalid response data type")
+	}
+
+	// Compare headers.
+	for key, expectedValue := range a.Headers {
+		value, ok := responseData.Headers[key]
+		if !ok || (len(value) > 0 && value[0] != expectedValue) {
+			return false, errors.New("header mismatch")
+		}
+	}
+
+	content, err := a.getBodyContent()
+	if err != nil {
+		return false, err
+	}
+
+	// Compare body content based on BodyMatch.
+	switch a.BodyMatch {
+	case MatchTypeExact:
+		if responseData.Body != content {
+			return false, errors.New("body content mismatch")
+		}
+	case MatchTypeRegexp:
+		matched, err := regexp.MatchString(content, responseData.Body)
+		if err != nil {
+			return false, err
+		}
+		if !matched {
+			return false, errors.New("body content does not match regexp")
+		}
+	}
 
 	return true, nil
+}
+
+func (a *ResponseAction) getBodyContent() (string, error) {
+	if a.BodyRenderTemplate {
+		content, err := a.Service.RenderTemplate(a.BodyContent)
+		if err != nil {
+			return "", err
+		}
+		return content, nil
+	}
+
+	return a.BodyContent, nil
 }
