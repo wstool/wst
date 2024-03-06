f9a01aff5b6df05ef98acc467bcb8e185e12507b
Jakub Zelenka <bukka@php.net>
2024-03-06 20:43:05 +0000

    Add few extra template parameters functions

--- run/services/template/parameters.go
+++ run/services/template/parameters.go
@@ -31,6 +31,17 @@ func (p Parameters) GetObject(key string) Parameters {
 	return Parameters{}
 }
 
+// GetObjectString retrieves a string value by the secondKey of the object retrieved by the firstKey
+func (p Parameters) GetObjectString(firstKey, secondKey string) string {
+	if val, ok := p[firstKey]; ok {
+		params := val.ToObject()
+		if val, ok = params[secondKey]; ok {
+			return val.ToString()
+		}
+	}
+	return ""
+}
+
 // Parameter is used to encapsulate values and provide a ToString method.
 type Parameter struct {
 	parameter parameter.Parameter
@@ -38,6 +49,18 @@ type Parameter struct {
 
 func NewParameter(parameter parameter.Parameter) *Parameter {
 	return &Parameter{parameter: parameter}
+}
+
+func (p *Parameter) IsNumber() bool {
+	paramType := p.parameter.Type()
+	return paramType == parameter.IntType || paramType == parameter.FloatType
+}
+
+func (p *Parameter) ToNumber() float64 {
+	if p.parameter == nil {
+		return 0.0
+	}
+	return p.parameter.FloatValue()
 }
 
 func (p *Parameter) ToString() string {
